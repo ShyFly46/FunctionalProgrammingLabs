4f5385766ecb84645a7b88e74f9c2a1bab35ec25
ShyFly46 <61005033+ShyFly46@users.noreply.github.com>
2022-11-27 23:51:02 +0800

    Update lab1.hs

--- lab1.hs
+++ lab1.hs
@@ -1,17 +1,17 @@
-findRoots :: (Float, Float, Float) -> [Float]
+data MyRoots = RNo | ROne Float | RTwo Float Float deriving (Show)
+
+findRoots :: (Float, Float, Float) -> MyRoots
 findRoots (a, b, c)
     | d >  0 = let f = sqrt d / (2*a)
-               in [e - f, e + f]
-    | d == 0 = [e]
-    | d <  0 = []
+               in RTwo (e - f) (e + f)
+    | d == 0 = ROne e
+    | d <  0 = RNo
     where d = b*b - 4*a*c
           e = -b / 2*a
 
 main = do
-    putStrLn "a, b, c?"
     aa <- getLine
     bb <- getLine
     cc <- getLine
     let polyn = (read aa :: Float, read bb :: Float, read cc :: Float)
-    putStr $ "roots = "
     print $ findRoots polyn
